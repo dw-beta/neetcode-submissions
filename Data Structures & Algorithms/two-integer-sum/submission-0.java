eb5bba4bc0fdd01e1bf9f187a180629792cb1677
dw-beta <84267457+dw-beta@users.noreply.github.com>
2026-08-14 19:13:09 +0700

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j] == complement) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
+
+// brute force
+// find complement by subtracting target with value in the first index
+// iterate through the array and find the complement
